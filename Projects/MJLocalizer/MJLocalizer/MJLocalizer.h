bb351ae70e05da08df33dbc88c0c7f4503b7219e
Martin Johannesson <mjohannesson@gmail.com>
2013-11-30 15:12:21 +0100

    Added framework project.

---
+++ Projects/MJLocalizer/MJLocalizer/MJLocalizer.h
@@ -0,0 +1,13 @@
+//
+//  MJLocalizer.h
+//  MJLocalizer
+//
+//  Created by Martin Johannesson on 2013-11-30.
+//  Copyright (c) 2013 Martin Johannesson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MJLocalizer : NSObject
+
+@end
---
+++ Projects/MJLocalizer/MJLocalizer/MJLocalizer.m
@@ -0,0 +1,13 @@
+//
+//  MJLocalizer.m
+//  MJLocalizer
+//
+//  Created by Martin Johannesson on 2013-11-30.
+//  Copyright (c) 2013 Martin Johannesson. All rights reserved.
+//
+
+#import "MJLocalizer.h"
+
+@implementation MJLocalizer
+
+@end
